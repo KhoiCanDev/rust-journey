e5680119cf2397d401f4f0dcdaa43e6608d53b11
Khoi Le <khoil@catavinsoft.com>
2023-03-06 12:01:06 +0700

    Upd 02 variables

--- c-02-variables/src/main.rs
+++ c-02-variables/src/main.rs
@@ -1,6 +1,29 @@
+use std::io;
+
 fn main() {
+    // variable and shadowing in action
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    // tuple in action
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {y}")
+
+    // array in action
+    let a = [1, 2, 3, 4, 5];
+    println!("Please enter an array index.");
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    let element = a[index];
+    // will panic when index is out of bound
+    println!("The value of the element at index {index} is: {element}");
 }
