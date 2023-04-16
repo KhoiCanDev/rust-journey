09a87e83611341e92435be22974977489d4d2bc1
KhoiCanDev <khoi.le.nt.1001@gmail.com>
2023-04-16 21:50:26 +0700

    Upd match case

--- c-07-struct/src/main.rs
+++ c-07-struct/src/main.rs
@@ -5,18 +5,6 @@ struct User {
     sign_in_count: u64,
 }
 
-#[derive(Debug)] // so we can inspect the state in a minute
-enum UsState {
-    Alabama,
-    Alaska,
-}
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 struct AlwaysEqual;
@@ -44,26 +32,4 @@ fn main() {
     //unit like struct
     //when need to implement a trait on some type but don't have any data that you want to store in the type
     let subject = AlwaysEqual;
-
-    // explain logic in match way
-    let coin = Coin::Quarter(UsState::Alabama);
-    let mut count = 0;
-    if let Coin::Quarter(state) = coin {
-        println!("State quarter from {:?}!", state);
-    } else {
-        count += 1;
-    }
-}
-
-// explain logics in match way, and exhaustion (every cases should be handled)
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
-    }
 }
--- c-08-enum/src/main.rs
+++ c-08-enum/src/main.rs
@@ -3,6 +3,18 @@ enum IpAddr {
     V6(String),
 }
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 // Define enum and its attached struct
 enum Message {
     Quit,
@@ -25,4 +37,26 @@ fn main() {
     let some_number = Some(5);
     let some_char = Some('e');
     let absent_number: Option<i32> = None;
+
+    // explain logic in match way
+    let coin = Coin::Quarter(UsState::Alabama);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
+
+// explain logics in match way, and exhaustion (every cases should be handled)
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
 }
